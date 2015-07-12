e71afcec8a2d93d909766baccc39839e393f00b5
Matthew Nguyen <contact@n-studio.fr>
2015-07-13 08:32:12 +0900

    fix production env

--- lib/templates/config/deploy/production.rb
+++ lib/templates/config/deploy/production.rb
@@ -23,7 +23,7 @@ set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 set :deploy_user, "deploy"
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
-set :rails_env, :staging
+set :rails_env, :production
 set :unicorn_worker_count, 5
 set :enable_ssl, true
 
