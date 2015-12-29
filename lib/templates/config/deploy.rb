7ef03e191caba4e1cea6fb03756c1db240d3270b
Matthew Nguyen <contact@n-studio.fr>
2015-12-29 23:57:14 +0700

    Updated to Ruby 2.3.0

--- lib/templates/config/deploy.rb
+++ lib/templates/config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:username/my_repository.git'
 set :secret_keys, [:secret_key_base, :db_name, :db_username, :db_password]
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
